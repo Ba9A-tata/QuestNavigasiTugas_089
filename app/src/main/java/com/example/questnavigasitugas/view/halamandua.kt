a98245207f503ab7b05b1d53e9ddc79b2b4cf5c5
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-07 19:57:13 +0700

    menambahkan scaffold dan kode untuk isi kontenya

--- app/src/main/java/com/example/questnavigasitugas/view/halamandua.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/halamandua.kt
@@ -8,4 +8,22 @@ fun HalamanSelamatDatang(
 ){
     val backgroundColor = Color(0xFFE0B0FF)
     val purpleButtonColor = Color(0xFF7B68EE)
-}
+    val textColor = Color(0xFF4B0082)
+
+    Scaffold(
+        containerColor = backgroundColor
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier
+                .padding(isiRuang)
+                .fillMaxSize()
+                .padding(horizontal = 30.dp, vertical = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold, color = textColor)
+
+                Spacer(modifier = Modifier.height(30.dp))
+
