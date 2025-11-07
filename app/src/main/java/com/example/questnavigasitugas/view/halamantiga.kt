1978a14485c8c437e9848d91e7d13fa1e05b376d
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-07 20:01:13 +0700

    menambahkanfun listdaftarpeserta

---
+++ app/src/main/java/com/example/questnavigasitugas/view/halamantiga.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigasitugas.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ListDaftarPeserta(
+
+    onBerandaClick: () -> Unit,
+    onFormulirPendaftaranClick: () -> Unit
+){
+
+}
