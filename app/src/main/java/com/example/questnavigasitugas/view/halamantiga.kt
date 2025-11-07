4450b053a250dbc7de93db388a0277ffc5ae8898
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-07 20:02:03 +0700

    menambahkan kode warna macam mcam

--- app/src/main/java/com/example/questnavigasitugas/view/halamantiga.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/halamantiga.kt
@@ -8,5 +8,9 @@ fun ListDaftarPeserta(
     onBerandaClick: () -> Unit,
     onFormulirPendaftaranClick: () -> Unit
 ){
+    val backgroundColor = Color(0xFFE0B0FF)
+    val cardColor = Color(0xFFF3E5F5)
+    val purpleButtonColor = Color(0xFF7B68EE)
+    val topBarColor = Color(0xFF9C27B0)
 
 }
