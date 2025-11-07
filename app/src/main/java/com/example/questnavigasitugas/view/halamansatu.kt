1e4043ea5a3d9ac238c16515e4eb1a59105c8e20
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-07 19:47:19 +0700

    menambahkan  fun DateApp

--- app/src/main/java/com/example/questnavigasitugas/view/halamansatu.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/halamansatu.kt
@@ -1,7 +1,17 @@
 package com.example.questnavigasitugas.view
 
+import androidx.compose.runtime.Composable
+
 enum class Navigasi{
     SelamatDatang,
     Formulirku,
     ListDetail
+}
+
+@Composable
+fun DateApp(
+    navController: NavHostController = rememberNavController(),
+    modifier : Modifier = Modifier
+){
+
 }
