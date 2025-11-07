8cc7c107166951a8e757e64699966dcc159e3d27
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-07 19:55:05 +0700

    menambahkan warna untuk background

--- app/src/main/java/com/example/questnavigasitugas/view/halamandua.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/halamandua.kt
@@ -1,8 +1,10 @@
 package com.example.questnavigasitugas.view
 
+import androidx.compose.runtime.Composable
+
 @Composable
 fun HalamanSelamatDatang(
     onSubmitClick: () -> Unit
 ){
-
+    val backgroundColor = Color(0xFFE0B0FF)
 }
